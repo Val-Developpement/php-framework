0d6ccfffcca50d0911fce123299da7561912451c
Val-Dev <anguillevalentin@gmail.com>
2024-11-30 01:01:07 +0100

    Exercice3 : method GET for one contact

--- app/src/Controllers/GetContactController.php
+++ app/src/Controllers/GetContactController.php
@@ -8,6 +8,7 @@ use App\Http\Response;
 use App\Manager\ContactManager;
 
 class GetContactController extends AbstractController {
+    
     public function process(Request $request): Response {
         $contactManager = new ContactManager;
         $contact = $contactManager->findOne($request->getArgs('filename'));
